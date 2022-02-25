bdf2cc51ae65d099838af7b7a0f886c15f30a73a
GitHub Action <action@github.com>
2022-02-25 10:37:23 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/renevanderark/myfirstideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.renevanderark.myfirstideaplugin.services
+
+import com.github.renevanderark.myfirstideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
